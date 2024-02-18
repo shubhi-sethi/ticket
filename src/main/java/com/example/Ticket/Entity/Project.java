3ee58dd0a83b3cd8fe9d4502ec5e7e2159354340
Shubhi <shubhis3g@gmail.com>
2024-02-18 16:52:34 +0530

    Commitingfiles for ticket

--- src/main/java/com/example/Ticket/Entity/Project.java
+++ src/main/java/com/example/Ticket/Entity/Project.java
@@ -1,16 +1,12 @@
 package com.example.Ticket.Entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Table(name="project")
 public class Project {
     @Id
@@ -22,4 +18,28 @@ public class Project {
 
     @Column(name = "description",nullable = false)
     private String description;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- src/main/java/com/example/Ticket/Entity/Ticket.java
+++ src/main/java/com/example/Ticket/Entity/Ticket.java
@@ -1,9 +1,7 @@
 package com.example.Ticket.Entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.sql.Timestamp;
@@ -11,8 +9,6 @@ import java.sql.Timestamp;
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Table(name="tickets")
 public class Ticket {
 
@@ -40,4 +36,68 @@ public class Ticket {
 
     @Column(name="created_on",nullable = true)
     private Timestamp createdOn;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getProjectIdFk() {
+        return projectIdFk;
+    }
+
+    public void setProjectIdFk(long projectIdFk) {
+        this.projectIdFk = projectIdFk;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getOwner() {
+        return owner;
+    }
+
+    public void setOwner(long owner) {
+        this.owner = owner;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public long getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(long createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Timestamp getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Timestamp createdOn) {
+        this.createdOn = createdOn;
+    }
 }
